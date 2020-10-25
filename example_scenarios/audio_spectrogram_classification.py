befc0f1e72cf81e775c428720091150bf72ac54d
Neal Gupta <neal.gupta@twosixlabs.com>
2020-10-25 16:42:52 -0400

    + Update scenario to use skip_benign, num_eval_batches + Move spectogram preprocessing into preprocessing defense + Update spectogram attack to be HopSkipJump

--- example_scenarios/audio_spectrogram_classification.py
+++ example_scenarios/audio_spectrogram_classification.py
@@ -18,36 +18,12 @@ from armory.utils.config_loading import (
     load_dataset,
     load_model,
     load_attack,
+    load_defense_internal,
 )
 from armory.utils import metrics
 from armory.scenarios.base import Scenario
 
 logger = logging.getLogger(__name__)
-
-
-def segment(x, y, n_time_bins):
-    """
-    Return segmented batch of spectrograms and labels
-
-    x is of shape (N,241,T), representing N spectrograms, each with 241 frequency bins
-    and T time bins that's variable, depending on the duration of the corresponding
-    raw audio.
-
-    The model accepts a fixed size spectrogram, so data needs to be segmented for a
-    fixed number of time_bins.
-    """
-
-    x_seg, y_seg = [], []
-    for xt, yt in zip(x, y):
-        n_seg = int(xt.shape[1] / n_time_bins)
-        xt = xt[:, : n_seg * n_time_bins]
-        for ii in range(n_seg):
-            x_seg.append(xt[:, ii * n_time_bins : (ii + 1) * n_time_bins])
-            y_seg.append(yt)
-    x_seg = np.array(x_seg)
-    x_seg = np.expand_dims(x_seg, -1)
-    y_seg = np.array(y_seg)
-    return x_seg, y_seg
 
 
 class AudioSpectrogramClassificationTask(Scenario):
@@ -58,31 +34,36 @@ class AudioSpectrogramClassificationTask(Scenario):
         Evaluate a config file for classification robustness against attack.
         """
         model_config = config["model"]
-        classifier, preprocessing_fn = load_model(model_config)
+        classifier, _ = load_model(model_config)
 
-        n_tbins = 100  # number of time bins in spectrogram input to model
+        defense_config = config.get("defense") or {}
+        defense_type = defense_config.get("type")
+
+        if defense_type in ["Preprocessor", "Postprocessor"]:
+            logger.info(f"Applying internal {defense_type} defense to classifier")
+            classifier = load_defense_internal(config["defense"], classifier)
 
         task_metric = metrics.categorical_accuracy
 
+        if config["dataset"]["batch_size"] != 1:
+            raise NotImplementedError("Currently only supports batch size of 1")
+
         # Train ART classifier
         if not model_config["weights_file"]:
+            raise NotImplementedError("Gradients not available for training.")
             classifier.set_learning_phase(True)
             logger.info(
                 f"Fitting model {model_config['module']}.{model_config['name']}..."
             )
             fit_kwargs = model_config["fit_kwargs"]
             train_data_generator = load_dataset(
-                config["dataset"],
-                epochs=fit_kwargs["nb_epochs"],
-                split_type="train",
-                preprocessing_fn=preprocessing_fn,
+                config["dataset"], epochs=fit_kwargs["nb_epochs"], split_type="train",
             )
 
             for cnt, (x, y) in tqdm(enumerate(train_data_generator)):
-                x_seg, y_seg = segment(x, y, n_tbins)
                 classifier.fit(
-                    x_seg,
-                    y_seg,
+                    x,
+                    y,
                     batch_size=config["dataset"]["batch_size"],
                     nb_epochs=1,
                     verbose=True,
@@ -91,71 +72,70 @@ class AudioSpectrogramClassificationTask(Scenario):
                 if (cnt + 1) % train_data_generator.batches_per_epoch == 0:
                     # evaluate on validation examples
                     val_data_generator = load_dataset(
-                        config["dataset"],
-                        epochs=1,
-                        split_type="validation",
-                        preprocessing_fn=preprocessing_fn,
+                        config["dataset"], epochs=1, split_type="validation",
                     )
 
                     cnt = 0
                     validation_accuracies = []
                     for x_val, y_val in tqdm(val_data_generator):
-                        x_val_seg, y_val_seg = segment(x_val, y_val, n_tbins)
-                        y_pred = classifier.predict(x_val_seg)
-                        validation_accuracies.extend(task_metric(y_val_seg, y_pred))
-                        cnt += len(y_val_seg)
+                        y_pred = np.mean(
+                            classifier.predict(x_val, batch_size=1),
+                            axis=0,
+                            keepdims=True,
+                        )
+                        validation_accuracies.extend(task_metric(y_val, y_pred))
+                        cnt += len(y_val)
                     validation_accuracy = sum(validation_accuracies) / cnt
                     logger.info("Validation accuracy: {}".format(validation_accuracy))
 
         classifier.set_learning_phase(False)
         # Evaluate ART classifier on test examples
-        logger.info(f"Loading testing dataset {config['dataset']['name']}...")
-        test_data_generator = load_dataset(
-            config["dataset"],
-            epochs=1,
-            split_type="test",
-            preprocessing_fn=preprocessing_fn,
-        )
+        if skip_benign:
+            logger.info("Skipping benign classification...")
+        else:
+            logger.info(f"Loading testing dataset {config['dataset']['name']}...")
+            test_data_generator = load_dataset(
+                config["dataset"],
+                epochs=1,
+                split_type="test",
+                num_batches=num_eval_batches,
+            )
+            logger.info("Running inference on benign test examples...")
 
-        logger.info("Running inference on benign test examples...")
+            cnt = 0
+            benign_accuracies = []
+            for x, y in tqdm(test_data_generator, desc="Benign"):
+                y_pred = np.mean(
+                    classifier.predict(x, batch_size=1), axis=0, keepdims=True
+                )
+                benign_accuracies.extend(task_metric(y, y_pred))
+                cnt += len(y)
 
-        cnt = 0
-        benign_accuracies = []
-        for x, y in tqdm(test_data_generator, desc="Benign"):
-            x_seg, y_seg = segment(x, y, n_tbins)
-            y_pred = classifier.predict(x_seg)
-            benign_accuracies.extend(task_metric(y_seg, y_pred))
-            cnt += len(y_seg)
-
-        benign_accuracy = sum(benign_accuracies) / cnt
-        logger.info(f"Accuracy on benign test examples: {benign_accuracy:.2%}")
+            benign_accuracy = sum(benign_accuracies) / cnt
+            logger.info(f"Accuracy on benign test examples: {benign_accuracy:.2%}")
 
         # Evaluate the ART classifier on adversarial test examples
         logger.info("Generating / testing adversarial examples...")
         attack = load_attack(config["attack"], classifier)
 
         test_data_generator = load_dataset(
-            config["dataset"],
-            epochs=1,
-            split_type="test",
-            preprocessing_fn=preprocessing_fn,
+            config["dataset"], epochs=1, split_type="test", num_batches=num_eval_batches
         )
 
         cnt = 0
         adversarial_accuracies = []
         for x, y in tqdm(test_data_generator, desc="Attack"):
-            x_seg, y_seg = segment(x, y, n_tbins)
-            x_adv = attack.generate(x=x_seg)
-            y_pred = classifier.predict(x_adv)
-            adversarial_accuracies.extend(task_metric(y_seg, y_pred))
-            cnt += len(y_seg)
+            x_adv = attack.generate(x=x)
+            y_pred = np.mean(
+                classifier.predict(x_adv, batch_size=1), axis=0, keepdims=True
+            )
+            adversarial_accuracies.extend(task_metric(y, y_pred))
+            cnt += len(y)
         adversarial_accuracy = sum(adversarial_accuracies) / cnt
         logger.info(
             f"Accuracy on adversarial test examples: {adversarial_accuracy:.2%}"
         )
-
-        results = {
-            "mean_benign_accuracy": benign_accuracy,
-            "mean_adversarial_accuracy": adversarial_accuracy,
-        }
+        results = {"mean_adversarial_accuracy": adversarial_accuracy}
+        if not skip_benign:
+            results["mean_benign_accuracy"] = benign_accuracy
         return results
