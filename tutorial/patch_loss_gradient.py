e7c9f8658663f7d3aa23bc304324457ce7df25c7
David Slater <david.slater@twosixlabs.com>
2021-07-19 22:23:14 -0700

    update tutorial

--- tutorial/patch_loss_gradient.py
+++ tutorial/patch_loss_gradient.py
@@ -13,15 +13,18 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class CustomAttack(ProjectedGradientDescent):
     def __init__(self, estimator, **kwargs):
 
-        # Create copy of the model (to avoid overwriting loss_gradient_framework of original model)
+        # Create copy of the model (to avoid overwriting loss_gradient of original model)
         new_estimator = get_art_model(model_kwargs={}, wrapper_kwargs={})
         new_estimator.model.load_state_dict(estimator.model.state_dict())
+        # OR:
+        # import copy
+        # new_estimator = copy.deepcopy(estimator)
 
         # Point attack to copy of model
         super().__init__(new_estimator, **kwargs)
 
         @patch_method(new_estimator)
-        def loss_gradient_framework(
+        def loss_gradient(
             self, x: "torch.Tensor", y: "torch.Tensor", **kwargs
         ) -> "torch.Tensor":
             x_var = Variable(x, requires_grad=True)
